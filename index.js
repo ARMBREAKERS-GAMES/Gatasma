abec243d30c89747054c17b0229ac720ba634fc9
Gustavo Rebelo <gustavo.pr2005@aluno.ifsc.edu.br>
2023-08-21 20:19:40 +0000

    Jogo mínimo: servidor (conteúdo estático) e cliente #17

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 /*importação de objetos*/
 
 import config from './config.js'
+import cena0 from './cena0.js'
 
 class Game extends Phaser.Game {
     constructor () {
         super(config)
+        
+        this.scene.add('cena0', cena0)
+        this.scene.start('cena0')
     }
 }
+
 
 window.onload = () => {
     window.game = new Game()
