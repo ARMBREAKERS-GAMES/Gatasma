990599a3f7bb41568e1428328c4e12a27864ac3d
melao roliso <vitor.h04@aluno.ifsc.edu.br>
2023-08-28 21:56:34 +0000

    Adcionado comentários e renomeando objeto jogador

--- cliente/js/cena0.js
+++ cliente/js/cena0.js
@@ -3,9 +3,10 @@ export default class cena0 extends Phaser.Scene {
     super('cena0')
   }
 
+  /*sprites*/
   preload () {
     this.load.image('Fundo', '../assets/Fundo.png')
-    this.load.spritesheet('gatoroubado', '../assets/gato1-inteiro.png', {
+    this.load.spritesheet('vivi', '../assets/gato1-inteiro.png', {
       frameWidth: 64,
       frameHeight: 64
     })
@@ -19,15 +20,21 @@ export default class cena0 extends Phaser.Scene {
       frameWidth: 64,
       frameHeight: 64
     })
+
+    this.load.spritesheet('cima', '../assets/cima.png', {
+      frameWidth: 64,
+      frameHeight: 64
+    })
   }
 
+  /*animações*/
   create () {
     this.add.image(400, 225, 'Fundo')
-    this.personagem = this.physics.add.sprite(400, 255, 'gatoroubado')
+    this.personagem = this.physics.add.sprite(400, 255, 'vivi')
 
     this.anims.create({
-      key: 'gatoroubado-parado',
-      frames: this.anims.generateFrameNumbers('gatoroubado', {
+      key: 'vivi-parado',
+      frames: this.anims.generateFrameNumbers('vivi', {
         start: 0,
         end: 0
       }),
@@ -35,8 +42,8 @@ export default class cena0 extends Phaser.Scene {
     })
 
     this.anims.create({
-      key: 'gatoroubado-direita',
-      frames: this.anims.generateFrameNumbers('gatoroubado', {
+      key: 'vivi-direita',
+      frames: this.anims.generateFrameNumbers('vivi', {
         start: 8,
         end: 11
       }),
@@ -45,8 +52,8 @@ export default class cena0 extends Phaser.Scene {
     })
 
     this.anims.create({
-      key: 'gatoroubado-esquerda',
-      frames: this.anims.generateFrameNumbers('gatoroubado', {
+      key: 'vivi-esquerda',
+      frames: this.anims.generateFrameNumbers('vivi', {
         start: 4,
         end: 7
       }),
@@ -54,17 +61,27 @@ export default class cena0 extends Phaser.Scene {
       repeat: -1
     })
 
+    this.anims.create({
+      key: 'vivi-cima',
+      frames: this.anims.generateFrameNumbers('vivi', {
+        start: 12,
+        end: 15
+      }),
+      frameRate: 6,
+      repeat: -1
+    })
 
+    /*botões*/
     this.direita = this.add.sprite(150, 400, 'direita', 0)
       .setInteractive()
       .on('pointerdown', () => {
         this.direita.setFrame(1)
-        this.personagem.anims.play('gatoroubado-direita', true)
+        this.personagem.anims.play('vivi-direita', true)
         this.personagem.setVelocityX(100)
       })
       .on('pointerup', () => {
         this.direita.setFrame(0)
-        this.personagem.anims.play('gatoroubado-parado')
+        this.personagem.anims.play('vivi-parado')
         this.personagem.setVelocityX(0)
       })
     
@@ -72,13 +89,26 @@ export default class cena0 extends Phaser.Scene {
       .setInteractive()
       .on('pointerdown', () => {
         this.esquerda.setFrame(1)
-        this.personagem.anims.play('gatoroubado-esquerda', true)
+        this.personagem.anims.play('vivi-esquerda', true)
         this.personagem.setVelocityX(-100)
       })
       .on('pointerup', () => {
         this.esquerda.setFrame(0)
-        this.personagem.anims.play('gatoroubado-parado')
+        this.personagem.anims.play('vivi-parado')
         this.personagem.setVelocityX(0)
+      })
+    
+    this.cima = this.add.sprite(700, 400, 'cima', 0)
+      .setInteractive()
+      .on('pointerdown', () => {
+        this.cima.setFrame(1)
+        this.personagem.anims.play('vivi-cima', true)
+        this.personagem.setVelocityY(-100)
+      })
+      .on('pointerup', () => {
+        this.cima.setFrame(0)
+        this.personagem.anims.play('vivi-parado')
+        this.personagem.setVelocityY(0)
       })
   }
 
