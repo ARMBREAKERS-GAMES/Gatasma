ea2c8e608934af0d6461b5166d7999d666337692
melao roliso <vitor.h04@aluno.ifsc.edu.br>
2023-08-28 20:04:20 +0000

    Botões e fundo adcionados

--- cliente/js/cena0.js
+++ cliente/js/cena0.js
@@ -4,23 +4,37 @@ export default class cena0 extends Phaser.Scene {
   }
 
   preload () {
-    this.load.image('ifsc-sj-2014', '../assets/ifsc-sj-2014.png')
+    this.load.image('Fundo', '../assets/Fundo.png')
     this.load.spritesheet('gatoroubado', '../assets/gato1-inteiro.png', {
+      frameWidth: 64,
+      frameHeight: 64
+    })
+
+    this.load.spritesheet('direita', '../assets/direita.png', {
+      frameWidth: 64,
+      frameHeight: 64
+    })
+
+    this.load.spritesheet('esquerda', '../assets/esquerda.png', {
       frameWidth: 64,
       frameHeight: 64
     })
   }
 
   create () {
-    this.add.image(400, 225, 'ifsc-sj-2014')
+    this.add.image(400, 225, 'Fundo')
     this.personagem = this.physics.add.sprite(400, 255, 'gatoroubado')
-      .setInteractive()
-      .on('pointerdown', () => { 
-        this.personagem.anims.play('gatoroubado-direita')
-        this.personagem.setVelocityX(100)
-      })
-    
-  this.anims.create({
+
+    this.anims.create({
+      key: 'gatoroubado-parado',
+      frames: this.anims.generateFrameNumbers('gatoroubado', {
+        start: 0,
+        end: 0
+      }),
+      frameRate: 1
+    })
+
+    this.anims.create({
       key: 'gatoroubado-direita',
       frames: this.anims.generateFrameNumbers('gatoroubado', {
         start: 8,
@@ -29,6 +43,43 @@ export default class cena0 extends Phaser.Scene {
       frameRate: 6,
       repeat: -1
     })
+
+    this.anims.create({
+      key: 'gatoroubado-esquerda',
+      frames: this.anims.generateFrameNumbers('gatoroubado', {
+        start: 4,
+        end: 7
+      }),
+      frameRate: 6,
+      repeat: -1
+    })
+
+
+    this.direita = this.add.sprite(150, 400, 'direita', 0)
+      .setInteractive()
+      .on('pointerdown', () => {
+        this.direita.setFrame(1)
+        this.personagem.anims.play('gatoroubado-direita', true)
+        this.personagem.setVelocityX(100)
+      })
+      .on('pointerup', () => {
+        this.direita.setFrame(0)
+        this.personagem.anims.play('gatoroubado-parado')
+        this.personagem.setVelocityX(0)
+      })
+    
+    this.esquerda = this.add.sprite(50, 400, 'esquerda', 0)
+      .setInteractive()
+      .on('pointerdown', () => {
+        this.esquerda.setFrame(1)
+        this.personagem.anims.play('gatoroubado-esquerda', true)
+        this.personagem.setVelocityX(-100)
+      })
+      .on('pointerup', () => {
+        this.esquerda.setFrame(0)
+        this.personagem.anims.play('gatoroubado-parado')
+        this.personagem.setVelocityX(0)
+      })
   }
 
   update () { }
