4a2bcef924e01085070848a6c35b9341003661b8
Gustavo Rebelo <gustavo.pr2005@aluno.ifsc.edu.br>
2023-09-07 06:31:50 +0000

    abertura adicionada

---
+++ cliente/js/abertura.js
@@ -0,0 +1,36 @@
+export default class abertura extends Phaser.Scene {
+  constructor () {
+    super('abertura')
+  }
+  preload() {
+    this.load.spritesheet('abertura', '../assets/capa.png', {
+      frameWidth: 800,
+      frameHeight: 450,
+    })
+  }
+    create() {
+      this.abertura = this.add.sprite(400, 225, 'abertura')
+      this.add.text(50, this.game.config.height * 0.85, '[iniciar]')
+        .setInteractive()
+        .on('pointerdown', () => {
+          this.game.scene.stop('abertura')
+          this.game.scene.start('labirinto')
+        })
+
+      /*Abertura*/
+
+      this.anims.create({
+        key: 'abertura',
+        frames: this.anims.generateFrameNumbers('abertura', {
+          start: 0,
+          end: 4
+        }),
+        frameRate: 3,
+        repeat: -1
+      })
+
+      this.abertura.anims.play('abertura')
+    }
+  }
+
+
--- cliente/js/index.js
+++ cliente/js/index.js
@@ -2,13 +2,15 @@
 
 import config from './config.js'
 import cena1 from './cena1.js'
+import abertura from './abertura.js'
 
 class Game extends Phaser.Game {
   constructor () {
     super(config)
 
     this.scene.add('cena1', cena1)
-    this.scene.start('cena1')
+    this.scene.add('aberura', abertura)
+    this.scene.start('abertura')
   }
 }
 
