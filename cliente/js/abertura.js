d6504aefc86a8259804b9d2e3be45e2614cfabee
Gustavo Rebelo <gustavo.pr2005@aluno.ifsc.edu.br>
2023-09-07 06:59:14 +0000

    abertura

--- cliente/js/abertura.js
+++ cliente/js/abertura.js
@@ -14,7 +14,7 @@ export default class abertura extends Phaser.Scene {
         .setInteractive()
         .on('pointerdown', () => {
           this.game.scene.stop('abertura')
-          this.game.scene.start('labirinto')
+          this.game.scene.start('cena1')
         })
 
       /*Abertura*/
